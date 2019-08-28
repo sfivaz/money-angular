75393824111f31be2dbdd4c050ccdd57bfa34574
sfivaz <s.a.fivaz@gmail.com>
2019-08-28 23:50:37 +0200

    fea - homepage created

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'm-homepage',
+  templateUrl: './homepage.component.html'
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
