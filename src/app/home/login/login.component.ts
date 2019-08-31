9e5be74509c01ae771236c68b191622688119b63
sfivaz <s.a.fivaz@gmail.com>
2019-08-31 19:54:20 +0200

    wip - Reactive form implemented for login

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './login.component.html',
@@ -6,9 +7,21 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    this.loginForm.get('email');
+    this.loginForm.get('password');
+    console.log("login...");
   }
 }
