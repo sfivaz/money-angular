c9f6ec3ed5335c7ab0dc2b35757e75ba225adca9
sfivaz <s.a.fivaz@gmail.com>
2019-09-01 00:27:03 +0200

    fea - filter methods in account / fea - momentJs installed

--- src/app/account/account.ts
+++ src/app/account/account.ts
@@ -1,4 +1,5 @@
 import {Transaction} from "../transaction/transaction";
+import * as moment from "moment";
 
 export class Account {
 
@@ -6,12 +7,14 @@ export class Account {
   private _name: string;
   private _transactions: Transaction[];
   private _balance: number;
+  private _budget: number;
 
   constructor(id, name, transactions, balance) {
     this._id = id;
     this._name = name;
     this._transactions = transactions || [];
     this._balance = Number(balance);
+    this._budget = 0;
   }
 
   get id() {
@@ -34,10 +37,6 @@ export class Account {
     return this._transactions;
   }
 
-  set transactions(value) {
-    this._transactions = value;
-  }
-
   get balance() {
     if (this._transactions.length) {
       const reducer = (total, transaction) => total + this.getValue(transaction);
@@ -56,13 +55,6 @@ export class Account {
     this.transactions.splice(index, 1);
   }
 
-  toJSON() {
-    return {
-      id: this.id,
-      name: this.name
-    };
-  }
-
   getValue(transaction) {
     switch (transaction.type) {
       case "income":
@@ -75,5 +67,77 @@ export class Account {
         if (this.id === transaction.sourceAccountId)
           return -1 * transaction.value;
     }
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      name: this.name
+    };
+  }
+
+  get budget(): number {
+    return this._budget;
+  }
+
+  set budget(value: number) {
+    this._budget = value;
+  }
+
+  get filteredBalance() {
+    const reducer = (total, transaction) => {
+      if (transaction.filteredBy.length > 0)
+        return total;
+      else
+        return total + this.getValue(transaction);
+    };
+    const balanceFiltered = this._transactions.reduce(reducer, 0) + this.budget;
+    return Number(balanceFiltered.toFixed(2));
+  }
+
+  filterMonths(month, year) {
+    const date = moment().month(month).year(year);
+    const firstDay = date.clone().startOf('month');
+    const lastDay = date.clone().endOf('month');
+
+    this._transactions.forEach(transaction => {
+      if (moment(transaction.date) < firstDay || moment(transaction.date) > lastDay)
+        transaction.addFilter("month");
+      else
+        transaction.removeFilter("month");
+    });
+  }
+
+  filterType(type) {
+    this._transactions.forEach(transaction => {
+      if (transaction.type !== type)
+        transaction.addFilter("type");
+      else
+        transaction.removeFilter("type");
+    });
+  }
+
+  filterCategory(category, useBudget) {
+    this._transactions.forEach(transaction => {
+      if (transaction.categoryId !== category.id)
+        transaction.addFilter("category");
+      else
+        transaction.removeFilter("category");
+    });
+    this.budget = useBudget ? category.budget : 0;
+  }
+
+  clearFilter(filter = null) {
+    //TODO filter == null? why not (!filter)
+    if (filter == null)
+      this.clearAllFilters();
+    else
+      this._transactions.forEach(transaction => transaction.removeFilter(filter));
+    if (filter === "category")
+      this.budget = 0;
+  }
+
+  clearAllFilters() {
+    this._transactions.forEach(transaction => transaction.filteredBy = []);
   }
 }
