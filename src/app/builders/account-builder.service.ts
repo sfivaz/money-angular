0fee727a465408c1ce7680468c06f16d063d978e
sfivaz <s.a.fivaz@gmail.com>
2019-09-01 00:25:32 +0200

    ref - accountService now returns instances of Account instead of plain objects

--- src/app/builders/account-builder.service.ts
+++ src/app/builders/account-builder.service.ts
@@ -10,6 +10,10 @@ export class AccountBuilderService {
   constructor(private transactionBuilder: TransactionBuilderService) {
   }
 
+  buildMany(accountsObj) {
+    return accountsObj.map(accountObj => this.build(accountObj));
+  }
+
   build(accountObj) {
     const transactions = this.transactionBuilder.buildMany(accountObj.transactions);
     return new Account(accountObj.id, accountObj.name, transactions, accountObj.balance);
