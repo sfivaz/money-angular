2cec5726c6e5543187baab5e59dc35f3385f51d0
sfivaz <s.a.fivaz@gmail.com>
2019-08-31 21:18:37 +0200

    fea - logout button implemented / ref - routes fixed

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {TokenService} from "../../services/token.service";
 
 @Component({
   selector: 'm-navbar',
@@ -7,17 +8,22 @@ import {Router} from "@angular/router";
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private tokenService: TokenService) {
   }
 
   ngOnInit() {
   }
 
   goToAccountsPage() {
-    this.router.navigateByUrl('');
+    this.router.navigateByUrl('home');
   }
 
   goToCategoriesPage() {
     this.router.navigateByUrl('categories');
+  }
+
+  logout() {
+    this.tokenService.removeToken();
+    this.router.navigateByUrl('');
   }
 }
