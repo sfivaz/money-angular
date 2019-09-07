a71b03bb8d8f60b4b2562d154986da555bcb3c43
sfivaz <s.a.fivaz@gmail.com>
2019-09-07 15:18:48 +0200

    ref - auth and guest guards moved to core/guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import {AccountsPageComponent} from "./accounts/accounts-page/accounts-page.comp
 import {AccountPageComponent} from "./account/account-page/account-page.component";
 import {CategoriesPageComponent} from "./categories/categories-page/categories-page.component";
 import {LoginComponent} from "./home/login/login.component";
-import {AuthGuard} from "./core/auth/auth.guard";
-import {GuestGuard} from "./core/guest/guest.guard";
+import {AuthGuard} from "./core/guards/auth.guard";
+import {GuestGuard} from "./core/guards/guest.guard";
 import {RegisterComponent} from "./home/register/register.component";
 
 const routes: Routes = [
