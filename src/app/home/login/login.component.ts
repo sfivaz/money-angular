0c3c323f754c1efc90c4c2e8e6c3708ecf87ca94
sfivaz <s.a.fivaz@gmail.com>
2019-09-07 15:15:20 +0200

    ref - login css moved to upper directory / ref - template of register component created

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -6,7 +6,7 @@ import {Router} from "@angular/router";
 
 @Component({
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['../login.component.css']
 })
 export class LoginComponent implements OnInit {
 
---
+++ src/app/home/register/register.component.ts
@@ -0,0 +1,50 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {TokenService} from "../../services/token.service";
+import {Router} from "@angular/router";
+import {User} from "../../core/user";
+
+@Component({
+  selector: 'm-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['../login.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+  failed = false;
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private tokenService: TokenService,
+              private route: Router) {
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  register() {
+    const user: User = {
+      id: null,
+      firstName: this.registerForm.get('firstName').value,
+      lastName: this.registerForm.get('lastName').value,
+      email: this.registerForm.get('email').value,
+      password: this.registerForm.get('password').value,
+    };
+
+    // this.authService.register(user)
+    //   .subscribe(response => {
+    //     if (response.status && response.status == 409) {
+    //       this.failed = true;
+    //     } else
+    //       this.route.navigateByUrl('/');
+    //   });
+  }
+}
