7bb98abd7a26159154324f34b6c7dffdd463eef1
sfivaz <s.a.fivaz@gmail.com>
2019-10-03 11:19:11 +0200

    imp - field value in transaction-form is always focused with directive focus

--- src/app/transaction/transaction-form/transaction-form.module.ts
+++ src/app/transaction/transaction-form/transaction-form.module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TransactionFormComponent} from './transaction-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {FocusDirective} from "../../shared/directives/focus.directive";
 
 @NgModule({
-  declarations: [TransactionFormComponent],
+  declarations: [
+    TransactionFormComponent,
+    FocusDirective,
+  ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   exports: [TransactionFormComponent]
 })
